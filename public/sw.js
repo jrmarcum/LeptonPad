4e4a2bc4a2228ce5ca274805685c21c9a5cfeab6
Jon Marcum <jrmarcum.se@gmail.com>
2026-04-21 14:21:24 -0400

    updates

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'leptonpad-v2.0.5';
+const CACHE = 'leptonpad-v2.0.6';
 const PRECACHE = ['/', '/main.js', '/main.css', '/solver.wasm', '/config.js', '/manifest.webmanifest', '/LeptonPadLogo.png'];
 
 self.addEventListener('install', (e) => {
